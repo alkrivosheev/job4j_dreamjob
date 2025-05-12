2beed8f20fe5a34ada7bf1a3704dbce666615dae
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2025-05-12 21:45:41 +0300

    Spring DI

--- src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleVacancyService.java
@@ -3,7 +3,6 @@ package ru.job4j.dreamjob.service;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.VacancyRepository;
-
 import java.util.Collection;
 import java.util.Optional;
 

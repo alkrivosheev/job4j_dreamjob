63ced5c8f2202d7ad0af12e9b0040e9ac22467d2
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2025-05-21 13:37:53 +0300

    add table Candidates

--- src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/SimpleCandidateService.java
@@ -4,7 +4,6 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.dto.FileDto;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.repository.CandidateRepository;
 import java.util.Collection;
 import java.util.Optional;
@@ -17,8 +16,8 @@ public class SimpleCandidateService implements CandidateService {
 
     private final FileService fileService;
 
-    public SimpleCandidateService(CandidateRepository candidateRepository, FileService fileService) {
-        this.candidateRepository = candidateRepository;
+    public SimpleCandidateService(CandidateRepository sql2oCandidateRepository, FileService fileService) {
+        this.candidateRepository = sql2oCandidateRepository;
         this.fileService = fileService;
     }
 
